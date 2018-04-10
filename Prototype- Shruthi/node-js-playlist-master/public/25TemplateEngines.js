1974536884cf8cc1c8b01772a7002d0bded78caf
ShruthiSiva <ssiva@github.com>
2018-04-09 21:22:15 -0400

    update

--- Prototype- Shruthi/node-js-playlist-master/public/25TemplateEngines.js
+++ Prototype- Shruthi/node-js-playlist-master/public/25TemplateEngines.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -9,7 +10,8 @@ app.get('/contact', function(req, res){
   res.sendFile(__dirname + '/contact.html');
 });
 
-app.get('/profile/:id', function(req, res){
-  res.send('You requested to see a profile with the id of ' + req.params.id);
+app.get('/profile/:name', function(req, res){
+  var data = {age: 22, job: 'Ninja'}
+  res.render('profile', {person: req.params.name, data:data});
 });
 app.listen(3000);
